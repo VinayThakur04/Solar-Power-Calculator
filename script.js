b167b6267617a4bcbee42a3a903ced17c3e0ad5a
Vinay Kumar Thakur <vinaykumar.thakur@lpu.in>
2023-04-09 12:57:21 +0530

    css file

---
+++ script.js
@@ -0,0 +1,9 @@
+function calculateSolarPower() {
+    var panelPower = document.getElementById("panelPower").value;
+    var panelNumber = document.getElementById("panelNumber").value;
+    var sunlightHours = document.getElementById("sunlightHours").value;
+    
+    var totalPower = panelPower * panelNumber * sunlightHours;
+    
+    document.getElementById("result").innerHTML = "Total Solar Power Generated: " + totalPower + " Watt-hours per day";
+  }
